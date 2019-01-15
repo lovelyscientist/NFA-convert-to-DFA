48975d87699beb87433f1c480e37374f5d7e1a8d
Avinash Singh <hiroc.singh@gmail.com>
2019-01-15 12:57:49 -0500

    Added Server.js

---
+++ automata/server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.use(express.static('public'));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.use(function(req, res) {
+    res.status(404).send("Page Not Found.");
+});
+
+app.listen(HTTP_PORT, onHttpStart);
