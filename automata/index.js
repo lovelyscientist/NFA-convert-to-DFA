c5140a4a319ff1cef52e7711fe51898925f72bbd
Avinash Singh <hiroc.singh@gmail.com>
2019-01-15 11:26:07 -0500

    Removed nonsense.

--- automata/index.js
+++
@@ -1,9 +0,0 @@
-// Include the server in your file
-const server = require('server');
-const { get } = server.router;
-
-// Handle requests to the url "/" ( http://localhost:3000/ )
-server(ctx => {
-    // ...
-    return 'index.html';
-  });
