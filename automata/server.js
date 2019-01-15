17f241b0335a20fe9139a944e50aed35bc4ec039
Avinash Singh <hiroc.singh@gmail.com>
2019-01-15 14:00:07 -0500

    Major modification for server.js.

--- automata/server.js
+++ automata/server.js
@@ -11,7 +11,7 @@ function onHttpStart() {
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/public/views/index.html"));
 });
 
 app.use(function(req, res) {
